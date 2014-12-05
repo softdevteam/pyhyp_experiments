9b8ae8b6500f08263361ca2c86dee1c19997131f
Edd Barrett <vext01@gmail.com>
2014-12-05 16:31:11 +0000

    Regenerate all benchmark composed variants.

--- benchmarks/heapsort/comp.php
+++ benchmarks/heapsort/comp.php
@@ -1,4 +1,4 @@
-<?php
+<?php{
 define ('IM', 139968);
 define ('IA', 3877);
 define ('IC', 29573);
@@ -11,12 +11,12 @@ function gen_random($n) {
 
 
 $__pyhyp__heapsort_r = embed_py_func("def __pyhyp__heapsort_r(n, ra):\n    l = (n >> 1) + 1\n    ir = n\n    \n    while True:\n\n        if l > 1:\n            l -= 1\n            rra = ra[l]\n        else:\n            rra = ra[ir]\n            ra[ir] = ra[1]\n            ir -= 1\n            if ir == 1:\n                ra[1] = rra\n                return\n        i = l\n        j = l << 1\n        while j <= ir:\n            if j < ir and ra[j] < ra[j+1]:\n                j += 1\n            if rra < ra[j]:\n                ra[i] = ra[j]\n                i = j\n                j += i\n            else:\n                j = ir + 1\n        ra[i] = rra");
-function heapsort_r(&$n, &$ra){
+function heapsort_r($n, $ra){
     global $__pyhyp__heapsort_r;
     return $__pyhyp__heapsort_r( $n, $ra);
 }
 
-function heapsort($N = 10) {
+function heapsort($N = 20000) {
   global $LAST;
 
   $ary = array(0);
@@ -29,4 +29,4 @@ function heapsort($N = 10) {
 }
 
 heapsort();
-?>
+}?>
