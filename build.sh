78242d32142ae97e50965f44d056b6040d9c5df7
Edd Barrett <vext01@gmail.com>
2014-12-18 15:02:43 +0000

    Add an XXX reminder.

--- build.sh
+++ build.sh
@@ -337,6 +337,9 @@ gen_config;
 
 # XXX install cffi someplace we can use it
 
+# XXX make wrapper scripts with environments set.
+# (e.g. LD_LIBRARY_PATH, PYTHONPATH)
+
 echo "We are done! Here are your interpreters:"
 echo "  HHVM:		${HHVM_BINARY}"
 echo "  CPYTHON:	${CPYTHON_BINARY}"
