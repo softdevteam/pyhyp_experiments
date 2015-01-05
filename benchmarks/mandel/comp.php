d8c7f088a72c38ce884f6e256cc29a64a8e76716
Edd Barrett <vext01@gmail.com>
2015-01-05 21:32:54 +0000

    Bugfix for composed mandel.

--- benchmarks/mandel/comp.php
+++ benchmarks/mandel/comp.php
@@ -1,12 +1,15 @@
 <?php{
+$H1 = 150;
 
-$__pyhyp__mandel = embed_py_func("def __pyhyp__mandel():\n    import sys\n    w1 = 50\n    h1=150\n    recen = -0.45\n    imcen = 0.0\n    r = 0.7\n    s = 0; rec = 0; imc = 0; re = 0; im = 0; re2 = 0; im2 = 0\n    x = 0; y = 0; w2 = 0; h2 = 0; color=0\n    s = 2*r/w1\n    w2 = 40\n    h2 = 12\n    for y in xrange(0, w1+1):\n        imc = s * (y-h2) + recen\n        for x in xrange(0, h1+1):\n            rec = s * (x-w2) + recen\n            re = rec\n            im = imc\n            color = 1000\n            re2 = re * re\n            im2 = im * im\n            while ((re2 + im2) < 1000000) and color > 0:\n                im = re * im*2 + imc\n                re = re2 - im2 + rec\n                re2 = re * re\n                im2 = im * im\n                color = color - 1\n            if color == 0:\n                sys.stdout.write(\"_\")\n            else:\n                sys.stdout.write(\"#\")\n        sys.stdout.write(\"\\n\")");
-function mandel(){
-    global $__pyhyp__mandel;
-    return $__pyhyp__mandel();
+
+$__pyhyp__mandel_py = embed_py_func("def __pyhyp__mandel_py(n):\n    lines = []\n\n    w1 = n\n    h1 = H1\n    recen = -0.45\n    imcen = 0.0\n    r = 0.7\n    s = 0\n    rec = 0\n    imc = 0\n    re = 0\n    im = 0\n    re2 = 0\n    im2 = 0\n    x = 0\n    y = 0\n    w2 = 0\n    h2 = 0\n    color = 0\n    s = 2 * r / w1\n    w2 = 40\n    h2 = 12\n\n    for y in xrange(w1 + 1):\n        line = []\n        imc = s * (y - h2) + imcen\n        for x in xrange(h1 + 1):\n            rec = s * (x - w2) + recen\n            re = rec\n            im = imc\n            color = 1000\n            re2 = re * re\n            im2 = im * im\n            while re2 + im2 < 1000000 and color > 0:\n                im = re * im * 2 + imc\n                re = re2 - im2 + rec\n                re2 = re * re\n                im2 = im * im\n                color = color - 1\n\n            if color == 0:\n                line.append(\"_\")\n            else:\n                line.append(\"#\")\n        lines.append(\"\".join(line))\n    out = \"\\n\".join(lines) + \"\\n\"\n    return out\n");
+function mandel_py($n){
+    global $__pyhyp__mandel_py;
+    return $__pyhyp__mandel_py( $n);
 }
-
 function run_iter($n){
-    mandel();
+    global $H1;
+    $out = mandel_py($n);
+    assert(strlen($out) == ($H1 + 2) * ($n + 1));
 }
 }?>
