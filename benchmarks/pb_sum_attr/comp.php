0419d092ad6633eed7498211fcf01483595d417d
Edd Barrett <vext01@gmail.com>
2015-01-28 15:02:22 +0000

    Translate pb_sum_attr.

---
+++ benchmarks/pb_sum_attr/comp.php
@@ -0,0 +1,28 @@
+<?php{
+// Same as pb_sum_meth, just using a attribute
+
+class Sum {
+	public $ct = 0;
+
+
+}
+embed_py_meth("Sum", "def f_rcv(self, a1, a2, a3, a4, a5):\n    self.ct += a1 + a2 + a3 + a4 + a5");
+
+function f_call($n) {
+	$s = new Sum();
+
+	for ($i = 0; $i < $n; $i++) {
+		$s->f_rcv($n + 1, $n + 2, $n + 3, $n + 4, $n + 5);
+	}
+	// each iteration adds 5n + 15
+	$expect = $n * (5 * $n + 15);
+
+	assert($s->ct == $expect);
+}
+
+function run_iter($n) {
+	f_call($n);
+}
+
+run_iter(1000000000);
+}?>
---
+++ benchmarks/pb_sum_attr/mono.py
@@ -0,0 +1,25 @@
+# Same as pb_sum_meth, just using a attribute
+
+class Sum(object):
+
+    def __init__(self):
+        self.ct = 0
+
+    def f_rcv(self, a1, a2, a3, a4, a5):
+        self.ct += a1 + a2 + a3 + a4 + a5
+
+def f_call(n):
+    s = Sum()
+
+    for i in xrange(n):
+        s.f_rcv(n + 1, n + 2, n + 3, n + 4, n + 5)
+
+    # each iteration adds 5n + 15
+    expect = n * (5 * n + 15)
+
+    assert s.ct == expect
+
+def run_iter(n):
+    f_call(n)
+
+run_iter(1000000000)
