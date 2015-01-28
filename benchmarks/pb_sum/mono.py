e4411cc9bce5880820ac763692ffaa606471d198
Edd Barrett <vext01@gmail.com>
2015-01-28 14:51:52 +0000

    typo

--- benchmarks/pb_sum/mono.py
+++ benchmarks/pb_sum/mono.py
@@ -7,7 +7,7 @@ def f_call(n):
     for i in xrange(n):
         ct += f_rcv(n + 1, n + 2, n + 3, n + 4, n + 5)
 
-    # each iteration adds 5n + 15 + i.
+    # each iteration adds 5n + 15
     expect = n * (5 * n + 15)
     assert ct == expect
 
