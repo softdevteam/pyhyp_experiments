40732450225e6700af8c8f066428045be8ba73f1
Edd Barrett <vext01@gmail.com>
2015-01-28 14:53:07 +0000

    Remove FP arithmetic from pb_sum_attr.

--- benchmarks/pb_sum_attr/mono.php
+++ benchmarks/pb_sum_attr/mono.php
@@ -5,8 +5,8 @@
 class Sum {
 	public $ct = 0;
 
-	function f_rcv($a1, $a2, $a3, $a4, $a5, $x) {
-		$this->ct += $a1 + $a2 + $a3 + $a4 + $a5 + $x;
+	function f_rcv($a1, $a2, $a3, $a4, $a5) {
+		$this->ct += $a1 + $a2 + $a3 + $a4 + $a5;
 	}
 }
 
@@ -14,11 +14,10 @@ function f_call($n) {
 	$s = new Sum();
 
 	for ($i = 0; $i < $n; $i++) {
-		$s->f_rcv($n + 1, $n + 2, $n + 3, $n + 4, $n + 5, $i);
+		$s->f_rcv($n + 1, $n + 2, $n + 3, $n + 4, $n + 5);
 	}
-	// each iteration adds 5n + 15 + $i.
-	// the sum of all of the $i we add is (i/2)(i-1).
-	$expect = (($i / 2) * ($i - 1)) + ($n * (5 * $n + 15));
+	// each iteration adds 5n + 15
+	$expect = $n * (5 * $n + 15);
 
 	assert($s->ct == $expect);
 }
