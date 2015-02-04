2ded2106c7748cee64a1161f06980006da0b5a54
Edd Barrett <vext01@gmail.com>
2015-02-04 14:08:27 +0000

    Remove debug from fannkuch.

--- benchmarks/fannkuch/mono.php
+++ benchmarks/fannkuch/mono.php
@@ -1,20 +1,6 @@
 <?php
 {
 
-// used to check that different variants execute the same program statements.
-function dbg_print_php() {
-	return;
-
-	$ar = func_get_args();
-	$len = func_num_args();
-
-	echo "{$ar[0]}: ";
-	for($i = 1; $i < $len; $i++) {
-		echo "{$ar[$i]} ";
-	}
-	echo "\n";
-}
-
 function Fannkuch_run($n){
 	$check = 0;
 	$perm = array();
@@ -27,24 +13,18 @@ function Fannkuch_run($n){
 	for ($i=0; $i<$n; $i++) $perm1[$i] = $i;
 	$r = $n;
 	while (TRUE) {
-		dbg_print_php("Outer loop");
 		while ($r != 1) {
-			dbg_print_php("Inner loop 1", $r);
 			$count[$r-1] = $r; $r--;
 		}
 		if (! ($perm1[0]==0 || $perm1[$m] == $m)){
-			dbg_print_php("Inner cond 1", $perm1[0], $perm1[$m], $m);
 			for($i=0; $i<$n; $i++) {
-				dbg_print_php("Inner loop 2", $i, $n);
 				$perm[$i] = $perm1[$i];
 			}
 			$flipsCount = 0;
 
 			while ( !(($k=$perm[0]) == 0) ) {
-				dbg_print_php("Inner loop 3", $k, $perm[0]);
 				$k2 = ($k+1) >> 1;
 				for($i=0; $i<$k2; $i++) {
-					dbg_print_php("Inner loop 4", $i, $k2);
 					$temp = $perm[$i];
 					$perm[$i] = $perm[$k-$i];
 					$perm[$k-$i] = $temp;
@@ -53,26 +33,20 @@ function Fannkuch_run($n){
 			}
 
 			if ($flipsCount > $maxFlipsCount) {
-				dbg_print_php("Inner cond 2",
-					$flipsCount, $maxFlipsCount);
 				$maxFlipsCount = $flipsCount;
 				for($i=0; $i<$n; $i++) {
-					dbg_print_php("Inner loop 5", $i, $n);
 					$maxPerm[$i] = $perm1[$i];
 				}
 			}
 		}
 
 		while (TRUE) {
-			dbg_print_php("Inner loop 6");
 			if ($r == $n) {
-				dbg_print_php("Inner cond 3", $r, $n);
 				return $maxFlipsCount;
 			}
 			$perm0 = $perm1[0];
 			$i = 0;
 			while ($i < $r) {
-				dbg_print_php("Inner loop 7", $i, $r);
 				$j = $i + 1;
 				$perm1[$i] = $perm1[$j];
 				$i = $j;
@@ -81,7 +55,6 @@ function Fannkuch_run($n){
 
 			$count[$r] = $count[$r] - 1;
 			if ($count[$r] > 0) {
-				dbg_print_php("Inner cond 4", $r);
 				break;
 			}
 			$r++;
@@ -91,7 +64,6 @@ function Fannkuch_run($n){
 
 function run_iter($n) {
 	$res = Fannkuch_run($n);
-	dbg_print_php("run_iter", $res);
 
 }
 
--- benchmarks/fannkuch/mono.py
+++ benchmarks/fannkuch/mono.py
@@ -1,12 +1,3 @@
-def dbg_print_py(func, *args):
-    return
-
-    import sys
-    sys.stdout.write("%s: " % func)
-    for i in args:
-        sys.stdout.write("%s " % i)
-    sys.stdout.write("\n")
-
 def Fannkuch_run(n):
     check = 0
     perm = [None for i in xrange(n)]
@@ -19,17 +10,13 @@ def Fannkuch_run(n):
 
     r = n
     while True:
-        dbg_print_py("Outer loop");
 
         while r != 1:
-            dbg_print_py("Inner loop 1", r)
             count[r-1] = r
             r -= 1
 
         if not (perm1[0] == 0 or perm1[m] == m):
-            dbg_print_py("Inner cond 1", perm1[0], perm1[m], m)
             for i in xrange(n):
-                dbg_print_py("Inner loop 2", i, n)
                 perm[i] = perm1[i]
 
             flipsCount = 0
@@ -40,34 +27,27 @@ def Fannkuch_run(n):
                 if k == 0:
                     break
 
-                dbg_print_py("Inner loop 3", k, perm[0])
 
                 k2 = (k + 1) >> 1
 
                 for i in xrange(k2):
-                    dbg_print_py("Inner loop 4", i, k2)
                     temp = perm[i]
                     perm[i] = perm[k-i]
                     perm[k - i] = temp
                 flipsCount += 1
 
             if flipsCount > maxFlipsCount:
-                dbg_print_py("Inner cond 2", flipsCount, maxFlipsCount)
                 maxFlipsCount = flipsCount
                 for i in xrange(n):
-                    dbg_print_py("Inner loop 5", i, n)
                     maxPerm[i] = perm1[i]
 
         while True:
-            dbg_print_py("Inner loop 6")
             if r == n:
-                dbg_print_py("Inner cond 3", r, n)
                 return maxFlipsCount
 
             perm0 = perm1[0]
             i = 0
             while i < r:
-                dbg_print_py("Inner loop 7", i, r)
                 j = i + 1
                 perm1[i] = perm1[j]
                 i = j
@@ -75,11 +55,10 @@ def Fannkuch_run(n):
 
             count[r] = count[r] - 1
             if count[r] > 0:
-                dbg_print_py("Inner cond 4", r)
                 break
 
             r += 1
 
 def run_iter(n):
     res = Fannkuch_run(n)
-    dbg_print_py("run_iter", res)
+
