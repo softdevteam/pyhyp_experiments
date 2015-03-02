32fcd3b910bc24411aae065cb63081d7e9d3b317
Edd Barrett <vext01@gmail.com>
2015-03-02 11:42:22 +0000

    Very basic latex table for absolute times.

--- confidence.py
+++ confidence.py
@@ -5,11 +5,15 @@ import sys
 import json
 import math
 import prettytable
+import os
 
 from pykalibera.data import Data
 
 CONF_SIZE = "0.99"  # intentionally str
 ITERATIONS = 1# 0000
+
+def tex_escape_underscope(s):
+    return s.replace("_", "\\_")
 
 def error(data):
     # lower, median, upper
@@ -59,6 +63,34 @@ def make_tables(config, data_file, latex_table_file):
         row_data[exp_key] = mean, err, warmup
 
     make_ascii_table(row_data)
+    make_latex_table(row_data, latex_table_file)
+
+def make_latex_table(row_data, latex_table_file):
+    with open(latex_table_file, "w") as of:
+
+        w = of.write
+
+        # header
+        w("""\\documentclass{article}
+        \\usepackage{booktabs}
+        \\usepackage{multicol}
+        \\usepackage{multirow}
+        \\usepackage{xspace}
+        \\usepackage{amsmath}
+        \\begin{document}\n""")
+
+        # absolute times
+        w("\\begin{tabular}{|r|r|r|}\n")
+        w("\\hline\n")
+        w("Key&Time (secs)& Error\\\\\n")
+        w("\\hline\n")
+        for exp_key, row_data in row_data.iteritems():
+            val, err, warmup = row_data
+            w("%s&  %6f& %6f\\\\\n" % (tex_escape_underscope(exp_key), val, err))
+
+        w("\\hline\n")
+        w("\\end{tabular}\n")
+        w("\\end{document}\n")
 
 def make_ascii_table(row_data):
 
@@ -85,11 +117,18 @@ if __name__ == "__main__":
 
     import_name = config_file[:-3]
     data_file = import_name + "_results.json"
-    latex_table_file = import_name + "_results.tex"
     try:
         config = __import__(import_name)
     except:
         print("*** error importing config file!\n")
         raise
+
+    ltx_dir = "tex"
+    try:
+        os.mkdir(ltx_dir)
+    except OSError as e:
+        pass # file exists
+
+    latex_table_file = os.path.join(ltx_dir, import_name + "_results.tex")
 
     make_tables(config, data_file, latex_table_file)
