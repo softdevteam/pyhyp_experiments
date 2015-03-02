641393c32be18c9256bd6dd11a7ef09212f002d7
Edd Barrett <vext01@gmail.com>
2015-03-02 16:12:22 +0000

    relative table.
    Looks awful mind.

--- confidence.py
+++ confidence.py
@@ -17,16 +17,29 @@ ITERATIONS = 1# 0000
 def tex_escape_underscope(s):
     return s.replace("_", "\\_")
 
+def avg(seq):
+    return math.fsum(seq) / len(seq)
+
 def error(data):
     # lower, median, upper
     a, b, c = data.bootstrap_confidence_interval(
         ITERATIONS,
         confidence=CONF_SIZE)
 
-    def avg(seq):
-        return math.fsum(seq) / len(seq)
-
     return avg([b - a, c - b])
+
+def rel(data, other):
+    a, b, c = data.bootstrap_quotient(
+        other, iterations=ITERATIONS, confidence=CONF_SIZE)
+    self_avg = data.mean()
+    other_avg = other.mean()
+
+    if other_avg == 0:
+        ratio = float("inf")
+    else:
+        ratio = self_avg / other_avg
+
+    return ratio, avg([b - a, c - b])
 
 def get_rowspan(row_data, bench, vm=None):
     ct = 0
@@ -42,12 +55,26 @@ def get_rowspan(row_data, bench, vm=None):
         ct += 1
     return ct
 
+def make_kalibera_data(exp_data, warmup):
+    data_arg = {}
+    total_execs = len(exp_data)
+    ok = False
+    for exec_n in range(total_execs):
+        exec_nowarmup = exp_data[exec_n][warmup - 1:]
+        if not exec_nowarmup:
+            return None
+        data_arg[(exec_n, )] = exec_nowarmup
+
+    return Data(data_arg, [total_execs, len(data_arg[(0, )])])
+
 def make_tables(config, data_file, latex_table_file):
 
     with open(data_file, "r") as data_fh:
         results = json.load(data_fh)["data"]
 
-    # bench -> vm -> variant -> val * abs_err * warmup
+    # convert to this tree-like dict, makes formatting tables easier
+    # bench -> vm -> variant ->
+    #    val * abs_err * rel_pypy * rel_pypy_err * rel_hip * rel_hip_err * warmup
     row_data = {}
 
     for bench_key, bench_param in config.BENCHMARKS.iteritems():
@@ -75,26 +102,44 @@ def make_tables(config, data_file, latex_table_file):
                 warmup = vm_info["warm_upon_iter"]
                 variant_info = config.VARIANTS[variant_key]
 
-                data_arg = {}
-                total_execs = len(exp_data)
-                ok = False
-                for exec_n in range(total_execs):
-                    exec_nowarmup = exp_data[exec_n][warmup - 1:]
-                    if not exec_nowarmup:
-                        print("\nMissing data for %s\n" % exp_key)
-                        break
-                    data_arg[(exec_n, )] = exec_nowarmup
-                else:
-                    ok = True
-
-                if not ok:
+                kdata = make_kalibera_data(exp_data, warmup)
+                if kdata is None:
+                    print("missing data for %s" % exp_key)
                     continue
-
-                kdata = Data(data_arg, [total_execs, len(data_arg[(0, )])])
                 mean = kdata.mean()
                 err = error(kdata)
 
-                row_data[bench_key][vm_key][variant_key] = mean, err, warmup
+                # relative to pypy
+                pypy_key = "%s:PyPy:mono-python" % bench_key
+                has_pypy = True
+                try:
+                    pypy_data = results[pypy_key]
+                except KeyError:
+                    has_pypy = False
+
+                if has_pypy:
+                    pypy_warmup = config.VMS["PyPy"]["warm_upon_iter"]
+                    pypy_kdata = make_kalibera_data(pypy_data, pypy_warmup)
+                    rel_pypy, rel_pypy_err = rel(kdata, pypy_kdata)
+                else:
+                    rel_pypy, rel_pypy_err = None, None
+
+                # relative to hippyvm
+                hippy_key = "%s:PyPy:mono-python" % bench_key
+                has_hippy = True
+                try:
+                    hippy_data = results[hippy_key]
+                except KeyError:
+                    has_hippy = False
+                if has_hippy:
+                    hippy_warmup = config.VMS["PyPy"]["warm_upon_iter"]
+                    hippy_kdata = make_kalibera_data(hippy_data, hippy_warmup)
+                    rel_hippy, rel_hippy_err = rel(kdata, hippy_kdata)
+                else:
+                    rel_hippy, rel_hippy_err = None, None
+
+                row_data[bench_key][vm_key][variant_key] = \
+                    mean, err, rel_pypy, rel_pypy_err, rel_hippy, rel_hippy_err, warmup
 
     # need to sort by key to ensure things work out
     row_data = collections.OrderedDict(sorted(row_data.items()))
@@ -114,7 +159,7 @@ def make_latex_table(row_data, latex_table_file):
         \\usepackage{multirow}
         \\usepackage{xspace}
         \\usepackage{amsmath}
-        \\begin{document}\n""")
+        \\begin{document}\small\n""")
 
         # absolute times
         w("\\begin{tabular}{cccrl}\n")
@@ -125,12 +170,12 @@ def make_latex_table(row_data, latex_table_file):
         for bench_key, bench_data in row_data.iteritems():
             for vm_key, vm_data in bench_data.iteritems():
                 for variant_key, variant_data in vm_data.iteritems():
-                    val, err, warmup = variant_data
+                    val, err, rel_pypy, rel_pypy_err, rel_hippy, rel_hippy_err, warmup = variant_data
 
-                    #vm_cline = True
+                    vm_cline = True
                     if last_bench_key != bench_key:
                         w("\\midrule\n")
-                        #vm_cline = False
+                        vm_cline = False
                         bench_rowspan = len(bench_data)
                         bench_cell = "\\multirow{%d}{*}{%s}" % (bench_rowspan, bench_key)
                         last_bench_key = bench_key
@@ -138,8 +183,8 @@ def make_latex_table(row_data, latex_table_file):
                         bench_cell = ""
 
                     if last_vm_key != vm_key:
-                        #if vm_cline:
-                        #    w("\\cline{2-5}\n")
+                        if vm_cline:
+                            w("\\cline{2-5}\n")
 
                         vm_rowspan = len(vm_data)
                         vm_cell = "\\multirow{%d}{*}{%s}" % (vm_rowspan, vm_key)
@@ -147,13 +192,72 @@ def make_latex_table(row_data, latex_table_file):
                     else:
                         vm_cell = ""
 
-                    w("%s&  %s& %s& %6f& {\scriptsize$\\pm$ %6f}\\\\\n" % (
+                    w("%s&  %s& %s& %.4f& {\scriptsize$\\pm$ %.4f}\\\\\n" % (
                         tex_escape_underscope(bench_cell),
                         tex_escape_underscope(vm_cell),
                         tex_escape_underscope(variant_key), val, err))
 
         w("\\bottomrule\n")
         w("\\end{tabular}\n")
+        w("\\newpage\n")
+
+
+        # relative times
+        w("\\begin{tabular}{cccrlrl}\n")
+        w("\\toprule\n")
+        w("Benchmark&   VM& Variant&    \multicolumn{2}{c}{$\\times$PyPy}&\multicolumn{2}{c}{$\\times$HippyVM}\\\\\n")
+
+        last_bench_key, last_vm_key = None, None
+        for bench_key, bench_data in row_data.iteritems():
+            for vm_key, vm_data in bench_data.iteritems():
+                for variant_key, variant_data in vm_data.iteritems():
+                    val, err, rel_pypy, rel_pypy_err, rel_hippy, rel_hippy_err, warmup = variant_data
+
+                    vm_cline = True
+                    if last_bench_key != bench_key:
+                        w("\\midrule\n")
+                        vm_cline = False
+                        bench_rowspan = len(bench_data)
+                        bench_cell = "\\multirow{%d}{*}{%s}" % (bench_rowspan, bench_key)
+                        last_bench_key = bench_key
+                    else:
+                        bench_cell = ""
+
+                    if last_vm_key != vm_key:
+                        if vm_cline:
+                            w("\\cline{2-7}\n")
+
+                        vm_rowspan = len(vm_data)
+                        vm_cell = "\\multirow{%d}{*}{%s}" % (vm_rowspan, vm_key)
+                        last_vm_key = vm_key
+                    else:
+                        vm_cell = ""
+
+                    if rel_pypy is not None:
+                        rel_pypy_str = "%.4f" % rel_pypy
+                        rel_pypy_err_str = "%.4f" % rel_pypy_err
+                    else:
+                        rel_pypy_str, rel_pypy_err_str = "n/a", "n/a"
+
+                    if rel_hippy is not None:
+                        rel_hippy_str = "%.4f" % rel_hippy
+                        rel_hippy_err_str = "%.4f" % rel_hippy_err
+                    else:
+                        rel_hippy_str, rel_hippy_err_str = "n/a", "n/a"
+
+                    w(("%s&  %s& %s&" + \
+                      "%s&{\scriptsize$\\pm$ %s}&" + \
+                      "%s&{\scriptsize$\\pm$ %s}\\\\\n") % (
+                        tex_escape_underscope(bench_cell),
+                        tex_escape_underscope(vm_cell),
+                        tex_escape_underscope(variant_key), rel_pypy_str,
+                        rel_pypy_err_str, rel_hippy_str, rel_hippy_err_str))
+
+        w("\\bottomrule\n")
+        w("\\end{tabular}\n")
+
+
+
         w("\\end{document}\n")
 
 def make_ascii_table(row_data):
