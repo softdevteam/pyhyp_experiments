8b48ca7fc610a5b741d04c4b72b75f814e7c8468
Edd Barrett <vext01@gmail.com>
2015-03-19 23:47:01 +0000

    Fix parse error in cffi example.

--- casestudies/cffi.php
+++ casestudies/cffi.php
@@ -6,20 +6,21 @@ $sys->exec_prefix = "";
 $cffi->prefix = "";
 $ffi = new $cffi->FFI();
 $ffi->cdef("double _clock_gettime_monotonic();");
-$C = $ffi->verify(<<<EOD
-    #include <time.h>
-    #include <math.h>
-    #include <stdlib.h>
-    double _clock_gettime_monotonic(){
-        struct timespec ts;
-        double result;
-        if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) < 0) {
-            perror("clock_gettime"); exit(1);
-        }
-        result = ts.tv_sec + ts.tv_nsec * pow(10, -9);
-        return (result);
+$csrc = <<<EOD
+  #include <time.h>
+  #include <math.h>
+  #include <stdlib.h>
+  double _clock_gettime_monotonic(){
+    struct timespec ts;
+    double result;
+    if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) < 0) {
+      perror("clock_gettime"); exit(1);
     }
+    result = ts.tv_sec + ts.tv_nsec * pow(10, -9);
+    return (result);
+  }
 EOD;
+$C = $ffi->verify($csrc);
 $time = $C->_clock_gettime_monotonic();
 echo "Monotonic time: " . $time;
 }
