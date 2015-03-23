f5a17a95eb0bb5280092757f812e41188b6c5e2e
Edd Barrett <vext01@gmail.com>
2015-03-23 16:08:42 +0000

    CFFI example changes from cfbolz.

--- casestudies/cffi.php
+++ casestudies/cffi.php
@@ -1,25 +1,32 @@
 <?php{
+
 $cffi = import_py_mod("cffi");
 $sys = import_py_mod("sys");
-$sys->prefix = "";
-$sys->exec_prefix = "";
-$cffi->prefix = "";
+$builtin = import_py_mod("__builtin__");
+$builtin->setattr($sys, "prefix", "");
+$builtin->setattr($sys, "exec_prefix", "");
+$builtin->setattr($cffi, "prefix", "");
+$verifier = import_py_mod("cffi.verifier");
+$verifier->set_tmpdir("/tmp");
 $ffi = new $cffi->FFI();
 $ffi->cdef("double _clock_gettime_monotonic();");
 $csrc = <<<EOD
-  #include <err.h>
   #include <time.h>
   #include <math.h>
   #include <stdlib.h>
   double _clock_gettime_monotonic(){
     struct timespec ts;
-    if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) == -1) {
-      err(1, "clock_gettime error");
+    double result;
+    if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) < 0) {
+      perror("clock_gettime"); exit(1);
     }
-    return ts.tv_sec + ts.tv_nsec * pow(10, -9);
+    result = ts.tv_sec + ts.tv_nsec * pow(10, -9);
+    return (result);
   }
 EOD;
 $C = $ffi->verify($csrc);
-echo "Monotonic time: " . $C->_clock_gettime_monotonic();
+$time = $C->_clock_gettime_monotonic();
+echo "Monotonic time: " . $time;
 }
+
 ?>
