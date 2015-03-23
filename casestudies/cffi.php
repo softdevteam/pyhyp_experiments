ba983eb3c912226c99c285d688b1ccac3a209167
Laurence Tratt <laurie@tratt.net>
2015-03-23 10:52:27 +0000

    More idiomatic error handling.

--- casestudies/cffi.php
+++ casestudies/cffi.php
@@ -7,13 +7,14 @@ $cffi->prefix = "";
 $ffi = new $cffi->FFI();
 $ffi->cdef("double _clock_gettime_monotonic();");
 $csrc = <<<EOD
+  #include <err.h>
   #include <time.h>
   #include <math.h>
   #include <stdlib.h>
   double _clock_gettime_monotonic(){
     struct timespec ts;
-    if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) < 0) {
-      perror("clock_gettime"); exit(1);
+    if ((clock_gettime(CLOCK_MONOTONIC_RAW, &ts)) == -1) {
+      err(1, "clock_gettime error");
     }
     return ts.tv_sec + ts.tv_nsec * pow(10, -9);
   }
